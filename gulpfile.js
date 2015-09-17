ab15b180e7ad25f86caa447d54d90dc34af2fd5a
Dan Elebash <danelebash@hotmail.com>
2015-09-16 21:25:00 -0400

    autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+
+// copies changed css files to the output directory
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dest'));
+});
+
+
+
+
+gulp.task('default', ['autoprefixer']);
